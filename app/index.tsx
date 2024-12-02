e5833af2f2fc77a5a04d278d9b110e9627126870
RizkiIqbal02 <roxashiqbal12@gmail.com>
2024-12-02 08:02:36 +0700

    menambah dependensi expo-gradient dan menggunakannya

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,34 @@
-import { View, Text, ActivityIndicator } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
 
 export default function StartUpPage() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ActivityIndicator size={50} />
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <LinearGradient
+                // Background Linear Gradient
+                colors={['#7F93E3', '#7B5DB0', '#66166D']}
+                style={styles.background}
+            />
+            <ActivityIndicator size={50} color={'#fff'} />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#7F93E3', // Fallback color
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,  // Mulai dari atas layar
+        bottom: 0, // Sampai bawah layar
+    },
+});
